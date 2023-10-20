d271d9b5f613194b8441f9eb6091e62b87d52ddd
m01ecu1e <molecule.g00gle@gmail.com>
2023-10-20 15:20:34 +0300

    laba1

--- app/src/main/java/com/example/laba1/HelloActivity.java
+++ app/src/main/java/com/example/laba1/HelloActivity.java
@@ -4,17 +4,30 @@ import androidx.annotation.Nullable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class HelloActivity extends Activity {
+    public int[] a = {0,0};
+    long timeLeft = 15000;
+    private TextView timerText;
+    private TextView text1;
+    CountDownTimer Timer1;
+    boolean timerRunning;
+    boolean timerStarted = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helloact);
 
-        TextView text1 = findViewById(R.id.text1);
-        final int[] a = {0,0};
+        text1 = findViewById(R.id.text1);
+        timerText = findViewById(R.id.timerText);
+        // = {0,0};
+
 
         Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +35,8 @@ public class HelloActivity extends Activity {
             public void onClick(View v) {
                 a[0]++;
                 button1.setText("Нажато");
-                text1.setText(String.valueOf(a[0])+","+String.valueOf(a[1]));
+                text1.setText(String.valueOf(a[0])+"  "+String.valueOf(a[1]));
+                if (!timerStarted) startTimer();
             }
         });
 
@@ -32,9 +46,56 @@ public class HelloActivity extends Activity {
             public void onClick(View v) {
                 a[1]++;
                 button2.setText("бим бом");
-                text1.setText(String.valueOf(a[0])+","+String.valueOf(a[1]));
+                text1.setText(String.valueOf(a[0])+"  "+String.valueOf(a[1]));
+                if (!timerStarted) startTimer();
             }
         });
 
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a[0] = 0;
+                a[1] = 0;
+                stopTimer();
+                text1.setText(String.valueOf(a[0])+"  "+String.valueOf(a[1]));
+                timeLeft = 15000;
+            }
+        });
+    }
+
+    public void stopTimer() {
+        Timer1.cancel();
+        timerRunning = false;
+        timerStarted = false;
+
+    }
+
+    public void updateTimer() {
+        int seconds = (int) timeLeft % 60000 / 1000;
+
+        timerText.setText(String.valueOf(seconds));
+    }
+
+    public void startTimer() {
+        Timer1 = new CountDownTimer(timeLeft,1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = l;
+                updateTimer();
+            }
+            @Override
+            public void onFinish() {
+                if (a[0] > 2 && a[1] > 2) {
+                    text1.setText("ПОБЕДА");
+                }else {
+                    text1.setText("GAMEOVER");
+                }
+                stopTimer();
+            }
+        }.start();
+        timerRunning = true;
     }
 }
+
+
